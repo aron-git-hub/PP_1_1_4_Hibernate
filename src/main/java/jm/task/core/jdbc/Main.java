8ef237492259d866cac9cfe6258017e3cb77bcad
aron <alexey.ronzhin@yandex.ru>
2023-03-07 02:20:26 +0300

    second commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,7 +2,6 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +11,8 @@ public class Main {
         userService.saveUser("John", "Cohen", (byte) 74);
         userService.saveUser("David", "Johnson", (byte) 47);
         userService.saveUser("Mark", "Davidson", (byte) 22);
-        userService.saveUser("Sara", "Davidson", (byte) 18);
+        userService.saveUser("Noah", "Davidson", (byte) 18);
 //        userService.removeUserById();
-        userService.dropUsersTable();
+//        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,10 +9,8 @@ import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Util util = new Util();
 
     public UserDaoJDBCImpl() {
-
     }
 
     public void createUsersTable() {
@@ -21,23 +19,22 @@ public class UserDaoJDBCImpl implements UserDao {
                     id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                     name VARCHAR(30) NOT NULL,
                     lastname VARCHAR(40) NOT NULL,
-                    age TINYINT NULL);""";
+                    age TINYINT NULL)""";
 
         try (Statement state = Util.getConnection().createStatement()) {
             state.executeUpdate(query);
             System.out.println("TABLE USER CREATED");
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void dropUsersTable() {
+        String query = "DROP TABLE IF EXISTS user";
 
-        String query = "DROP TABLE IF EXISTS user;";
-
-        try(Statement state = Util.getConnection().createStatement()) {
+        try (Statement state = Util.getConnection().createStatement()) {
             state.executeUpdate(query);
-
             System.out.println("TABLE USER DROPPED");
 
         } catch (SQLException e) {
@@ -46,16 +43,14 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String query = "INSERT INTO user (name, lastname, age) VALUES(?, ?, ?)";
 
-        String query = "INSERT INTO user (name, lastname, age) VALUES(?, ?, ?);";
-
-        try(PreparedStatement pState = Util.getConnection().prepareStatement(query)) {
+        try (PreparedStatement pState = Util.getConnection().prepareStatement(query)) {
             pState.setString(1, name);
             pState.setString(2, lastName);
             pState.setByte(3, age);
 
-            pState.executeUpdate(query);
-
+            pState.executeUpdate();
             System.out.println("USER SAVED");
 
         } catch (SQLException e) {
@@ -64,12 +59,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        String query = "DELETE FROM user WHERE id = ?;";
-        try(PreparedStatement pState = Util.getConnection().prepareStatement(query)) {
+        String query = "DELETE FROM user WHERE id = ?";
+        try (PreparedStatement pState = Util.getConnection().prepareStatement(query)) {
             pState.setLong(1, id);
 
-            pState.executeUpdate(query);
-
+            pState.executeUpdate();
             System.out.println("USER DELETED");
 
         } catch (SQLException e) {
